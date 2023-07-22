f0e724c8dd51a8c95d072a63e85b6c8cadc4eb01
HxSimo <HxSimo@proton.me>
2023-07-22 21:59:55 +0200

    Melon contracts in progress

--- smarcontractAbstraction/contracts/Melon.sol
+++ smarcontractAbstraction/contracts/Melon.sol
@@ -2,25 +2,48 @@
 pragma solidity ^0.8.9;
 
 import "../lib/sismo-connect-solidity/src/SismoConnectLib.sol"; 
+import "../node_modules/@openzeppelin/contracts/access/Ownable.sol";
 
-contract Melon is SismoConnect {
+contract Melon is Ownable, SismoConnect {
 
-    constructor()
+    bytes16 immutable groupId;
+    mapping(uint => mapping(uint => uint)) public actionDone;
+    mapping(uint => uint) public maxActionById;
+    mapping(uint => uint) public minRank;
+    mapping(uint => address) public contracts;
+    uint256 public lastResult; 
+    uint256 public lastAuth;
+
+    constructor(bytes16 _groupId, address _owner)
+        Ownable()
         SismoConnect(
             buildConfig({
                 appId: 0x4b23209e82c689fad20b91d2331e6668
             })
         )
-    {}
-
-    function action(bytes memory response, bytes16 _groupId, address _wallet) public returns (SismoConnectVerifiedResult memory result) {
-        result = verify({
-            responseBytes: response,
-            claim: buildClaim({groupId: _groupId}),
-            signature: buildSignature({message: abi.encode(_wallet)})
-        });
+    {
+        groupId = _groupId;
+        transferOwnership(_owner);
     }
 
+    function melonAction(bytes memory response, address _wallet, uint _id) external returns (SismoConnectVerifiedResult memory result) {
+        result = verify({
+            responseBytes: response,
+            claim: buildClaim({groupId: groupId}),
+            signature: buildSignature({message: abi.encode(_wallet)})
+        });
+        require(minRank[_id] <= result.claims[0].value, "ERROR: your rank is too low");
+        require(actionDone[_id][result.auths[0].userId] < maxActionById[_id], "ERROR: you reached the maxActions for this id");
+        actionDone[_id][result.auths[0].userId]++;
+        
+    }
+
+    function giveAction(uint _id, uint _minRank, uint _maxNumberActions, address _contract) external onlyOwner {
+        require(_minRank >= 1, "ERROR: can't be lower than 1");
+        maxActionById[_id] = _maxNumberActions;
+        minRank[_id] = _minRank;
+        contracts[_id] = _contract;
+    }
 }
 
 /*
