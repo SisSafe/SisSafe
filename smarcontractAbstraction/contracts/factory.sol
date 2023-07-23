c04677c250654183ebf606850cf328b75d01e932
HxSimo <HxSimo@proton.me>
2023-07-23 05:31:01 +0200

    Help me

--- smarcontractAbstraction/contracts/factory.sol
+++ smarcontractAbstraction/contracts/factory.sol
@@ -7,10 +7,8 @@ contract MelonFactory {
 
     event MelonContractCreated(address indexed MelonContractAddress, address indexed creator);
 
-    function createChildContract(bytes16 _appId, bytes16 _groupId, address _owner) external {
+    function createMelonContract(bytes16 _appId, bytes16 _groupId, address _owner) external {
         Melon MelonContract = new Melon(_appId, _groupId, _owner);
-
-    
         emit MelonContractCreated(address(MelonContract), msg.sender);
     }
 }
